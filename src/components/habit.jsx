d585f67a7f46701002493036b187f841cf7c9ea9
Yunsu <kenatman0723@gmail.com>
2021-07-02 15:39:22 +0900

    4.7 State 이해하기

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
 
 class Habit extends Component {
+  state = { count: 0 };
+
+  handleIncrement = () =>
+    this.setState((curr) => ({
+      count: curr.count + 1,
+    }));
+
+  handleDecrement = () =>
+    this.setState((curr) => ({
+      count: curr.count - 1 < 0 ? 0 : curr.count - 1,
+    }));
+
   render() {
     return (
       <li className="habit">
         <span className="habit-name">Reading</span>
-        <span className="habit-count">8</span>
-        <button className="habit-button habit-inrease">
+        <span className="habit-count">{this.state.count}</span>
+        <button
+          className="habit-button habit-inrease"
+          onClick={this.handleIncrement}
+        >
           <i className="fas fa-plus-square"></i>
         </button>
-        <button className="habit-button habit-decrease">
+        <button
+          className="habit-button habit-decrease"
+          onClick={this.handleDecrement}
+        >
           <i className="fas fa-minus-square"></i>
         </button>
         <button className="habit-button habit-delete">
