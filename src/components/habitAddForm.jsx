838b6710496f159954719f4cf4b93b648f5fc565
Yunsu <kenatman0723@gmail.com>
2021-07-05 14:33:09 +0900

    4.12 Ref

---
+++ src/components/habitAddForm.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+class HabitAddForm extends Component {
+  formRef = React.createRef();
+  inputRef = React.createRef();
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const name = this.inputRef.current.value;
+    name && this.props.onAdd(name);
+    this.formRef.current.reset();
+    // 인풋 초기화 다른방법...
+    // this.inputRef.current.value = "";
+  };
+
+  render() {
+    return (
+      <form
+        ref={this.formRef}
+        className="add-form"
+        onSubmit={this.handleSubmit}
+      >
+        <input
+          ref={this.inputRef}
+          type="text"
+          className="add-input"
+          placeholder="Enter your habits..."
+        ></input>
+        <button className="add-button">Add</button>
+      </form>
+    );
+  }
+}
+
+export default HabitAddForm;
