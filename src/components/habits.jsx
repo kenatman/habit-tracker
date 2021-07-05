3913717c29ce489de300560aa7c40217b29c4482
Yunsu <kenatman0723@gmail.com>
2021-07-05 11:56:26 +0900

    4.11 Navbar 컴포넌트 만들기

--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -2,41 +2,22 @@ import React, { Component } from "react";
 import Habit from "./habit";
 
 class Habits extends Component {
-  state = {
-    habits: [
-      { id: 1, name: "Reading", count: 0 },
-      { id: 2, name: "Running", count: 0 },
-      { id: 3, name: "Coding", count: 0 },
-    ],
-  };
-
   handleIncrement = (habit) => {
-    console.log(`handleIncrement ${habit.name}`);
-    const habits = [...this.state.habits];
-    const index = habits.lastIndexOf(habit);
-    habits[index].count++;
-    this.setState({ habits });
+    this.props.onIncrement(habit);
   };
 
   handleDecrement = (habit) => {
-    console.log(`handleDecrement ${habit.name}`);
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    const count = habits[index].count - 1;
-    habits[index].count = count < 0 ? 0 : count;
-    this.setState({ habits });
+    this.props.onDecrement(habit);
   };
 
   handleDelete = (habit) => {
-    console.log(`handleDelete ${habit.name}`);
-    const habits = this.state.habits.filter((item) => item.id !== habit.id);
-    this.setState({ habits });
+    this.props.onDelete(habit);
   };
 
   render() {
     return (
       <div>
-        {this.state.habits.map((habit) => (
+        {this.props.habits.map((habit) => (
           <Habit
             key={habit.id}
             habit={habit}
